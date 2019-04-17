a56254dad6ac196811cb751aff9f3ce9c46e9bf0
Juliana JEAN-MARIE <JJean-marie@oui.sncf>
2019-04-17 13:40:51 +0200

    split webpack conf + babel class support

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
@@ -14,7 +13,8 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
                 }
             }
         ]
