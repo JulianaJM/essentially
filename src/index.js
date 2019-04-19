13f9fa58decd6d3fb6c6fdc0854eaf3f29de67a6
Juliana JEAN-MARIE <JJean-marie@oui.sncf>
2019-04-19 23:59:33 +0200

    error boundarie

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill'; // polyfill features not available in older browsers (eg promise)
 import App from './App';
+import DefaultErrorBoundary from './DefaultErrorBoundary';
 import './style.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 );
