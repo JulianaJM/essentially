f6f4b07d33171db371c830729994721e41ababf7
Juliana JEAN-MARIE <JJean-marie@oui.sncf>
2019-04-19 22:58:51 +0200

    init prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import '@babel/polyfill'; // polyfill features not available in older browsers (eg promise)
-import App from "./App"
-import "./style.css"
+import App from './App';
+import './style.css';
 
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
