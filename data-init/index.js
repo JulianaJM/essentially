bedb9d24b10decc1ac320829d9de31053dd9324a
Juliana JEAN-MARIE <JJean-marie@oui.sncf>
2019-05-19 23:36:01 +0200

    scrapping data begining

---
+++ data-init/index.js
@@ -0,0 +1,133 @@
+var request = require('request-promise');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+const getLinks = (url, patern, next) => {
+  request(url, function(err, resp, html) {
+    if (!err) {
+      const links = [];
+      const $ = cheerio.load(html);
+      $(`a[href*="${patern}"]`).each((index, value) => {
+        var link = $(value).attr('href');
+        links.push(link);
+      });
+      next(null, links);
+    }
+  });
+};
+
+const promiseRequest = url => {
+  const healthSymptoms = [];
+  const moodSymptoms = [];
+  const beautySymptoms = [];
+
+  const options = {
+    uri: url,
+    transform: function(body) {
+      return cheerio.load(body);
+    }
+  };
+  return request(options).then($ => {
+    // title
+    const name = $('h1').text();
+
+    // description
+    const description = $('.row .doc-content')
+      .children('div.col-md-18')
+      .children('p')
+      .first()
+      .text();
+
+    // img
+    const picture = $('.doc-right').prop('src');
+
+    // symptoms
+    healthSymptoms.push(...getSymptoms($, 15));
+    moodSymptoms.push(...getSymptoms($, 16));
+    beautySymptoms.push(...getSymptoms($, 17));
+    return {
+      name,
+      description,
+      picture,
+      goodFor: [...healthSymptoms, ...moodSymptoms, ...beautySymptoms]
+    };
+  });
+};
+
+const promiseSymptomsRequest = url => {
+  const healthSymptoms = [];
+  const moodSymptoms = [];
+  const beautySymptoms = [];
+
+  const options = {
+    uri: url,
+    transform: function(body) {
+      return cheerio.load(body);
+    }
+  };
+  return request(options).then($ => {
+    // symptoms
+    healthSymptoms.push(...getSymptoms($, 15));
+    moodSymptoms.push(...getSymptoms($, 16));
+    beautySymptoms.push(...getSymptoms($, 17));
+    return (
+      {
+        category: 'Health',
+        list: [...healthSymptoms]
+      },
+      {
+        category: 'Mood',
+        list: [...moodSymptoms]
+      },
+      {
+        category: 'Beauty',
+        list: [...beautySymptoms]
+      }
+    );
+  });
+};
+
+const getDataFromLinks = links => {
+  return Promise.all(links.map(link => promiseRequest(link)));
+};
+
+const getDataSymptomsFromLinks = links => {
+  return Promise.all(links.map(link => promiseSymptomsRequest(link)));
+};
+
+const getSymptoms = ($, index) => {
+  return $('ul')
+    .eq(index)
+    .children('li')
+    .map(function(i, element) {
+      return $(element)
+        .text()
+        .trim();
+    })
+    .get()
+    .join(',')
+    .split(',');
+};
+
+const saveData = data => {
+  fs.writeFileSync('../src/resources/data.json', JSON.stringify(data));
+};
+const saveDataSymptoms = data => {
+  fs.writeFileSync('../src/resources/data1.json', JSON.stringify(data));
+};
+getLinks(
+  'http://www.doctissimo.fr/sante/aromatherapie/guide-huiles-essentielles',
+  'http://www.doctissimo.fr/sante/aromatherapie/guide-huiles-essentielles/huile-essentielle-',
+  (err, response) => {
+    if (err) {
+      throw new Error('une erreur est survenue');
+    }
+    getDataFromLinks(response).then(response => {
+      saveData(response);
+    });
+
+    getDataSymptomsFromLinks(response).then(response => {
+      saveDataSymptoms(response);
+    });
+  }
+);
