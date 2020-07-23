22210ea82a6f78442badf2a6edde03a460e1ff3b
Juliana <juliana.jeanmarie972@gmail.com>
2020-07-23 22:31:37 +0200

    btn reset input

--- src/page/SearchPage.jsx
+++ src/page/SearchPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import throttle from "lodash/throttle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
 import SearchBox from "../components/search/searchbox/SearchBox";
 import SearchResults from "../components/search/SearchResults";
@@ -24,6 +26,10 @@ const SearchPage = props => {
 
   const handleScroll = () => {
     setIsBottom(isPageBottom());
+  };
+
+  const onResetInput = () => {
+    setValue("");
   };
 
   const trottledFunction = throttle(handleScroll, 500);
@@ -55,6 +61,9 @@ const SearchPage = props => {
     <>
       <div className="search-bar">
         <SearchBox onUpdate={handleChange} value={value} />
+        <button type="button" className="reset-btn" onClick={onResetInput}>
+          <FontAwesomeIcon icon={faTimesCircle} />
+        </button>
       </div>
       <SearchResults isPageBottom={isBottom} {...props} />
     </>
